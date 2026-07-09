f0dd8d1c98ae40049960b2ce991d1ed9c3f8c001
Deepak Karthikeyan <deepak.karthikeyan0612@gmail.com>
2026-07-09 23:32:10 +0100

    "feature-domain": added base entity, category, brand and product

---
+++ backend/TechForge.Domain/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using TechForge.Domain.Common;
+
+namespace TechForge.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
