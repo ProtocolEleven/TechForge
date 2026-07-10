bede539aefd2dba7f368305054ae97698444e63f
Deepak Karthikeyan <deepak.karthikeyan0612@gmail.com>
2026-07-11 00:41:52 +0100

    Feature (Database): Created initial Migration
    Added EF Core Packages | Application DB Context | Fluent Configuration

---
+++ backend/TechForge.Infrastructure/Data/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechForge.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechForge.Infrastructure.Data.Configurations
+{
+    public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+            builder.HasIndex(c => c.Name)
+                    .IsUnique();
+
+            builder.Property(c => c.Description)
+                    .HasMaxLength(500);
+
+            builder.Property(c => c.IsActive)
+                    .HasDefaultValue(true);
+
+            builder.HasMany(c => c.Products)
+                    .WithOne(c => c.Category)
+                    .HasForeignKey(c => c.CategoryId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ backend/TechForge.Infrastructure/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechForge.Domain.Entities;
+
+namespace TechForge.Infrastructure.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Brand> Brands => Set<Brand>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly );
+        }
+            
+        
+    }
+}
