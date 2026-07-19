342b17e505db9808951a9fdc900008f88e26f8ca
Deepak Karthikeyan <deepak.karthikeyan0612@gmail.com>
2026-07-20 00:09:33 +0100

    feat-Repository: Implement product repository

--- backend/TechForge.Application/DependencyInjection.cs
+++ backend/TechForge.Application/DependencyInjection.cs
@@ -9,7 +9,6 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(
         this IServiceCollection services)
     {
-        services.AddAutoMapper(typeof(MappingProfile));
 
         return services;
     }
