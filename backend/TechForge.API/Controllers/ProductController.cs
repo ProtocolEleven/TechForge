eb662e338bcaff4ed7a6f85fdf60526ba22b202c
Deepak Karthikeyan <deepak.karthikeyan0612@gmail.com>
2026-07-24 01:07:09 +0100

    feat(product): implement CRUD API for product management

---
+++ backend/TechForge.API/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechForge.Application.DTOs;
+using TechForge.Application.Services.Interfaces;
+
+namespace TechForge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id: int}")]
+        public async Task<ActionResult<ProductDto>> GetById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+
+            if (product == null) { return NotFound(); }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(CreateProductDto dto)
+        {
+            var id = await _productService.CreateAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id },
+                null);
+        }
+
+        [HttpPut("{id: int}")]
+        public async Task<IActionResult> UpdateProduct(int id, UpdateProductDto dto)
+        {
+            await _productService.UpdateAsync(id, dto);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id: int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
--- backend/TechForge.API/Program.cs
+++ backend/TechForge.API/Program.cs
@@ -1,4 +1,5 @@
 using TechForge.Infrastructure.Extensions;
+using TechForge.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
