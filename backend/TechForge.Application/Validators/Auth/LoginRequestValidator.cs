bbb6d076147f330e38196a2be8588ebeec52c608
Deepak Karthikeyan <deepak.karthikeyan0612@gmail.com>
2026-09-02 01:48:26 +0100

    add request validation

---
+++ backend/TechForge.Application/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TechForge.Application.DTOs.Auth;
+
+namespace TechForge.Application.Validators.Auth;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
